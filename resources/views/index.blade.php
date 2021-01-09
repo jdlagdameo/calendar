58988e92010773a0b8e6e4d46c1dbb6780bc35d5
John Dave Lagdameo <jdlagdameo@pldtglobal.com>
2021-01-09 22:38:51 +0800

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
-    <title>Calendar</title>
+    <title>Calendar Test</title>
 </head>
 <body>
     <div id="app">
