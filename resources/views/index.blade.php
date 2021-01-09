1715849dfd76b8a27d2ce1f1dc95f1fbdf000a2a
John Dave Lagdameo <jdlagdameo@pldtglobal.com>
2021-01-09 22:24:46 +0800

    change page title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
-    <title>Document</title>
+    <title>Calendar</title>
 </head>
 <body>
     <div id="app">
