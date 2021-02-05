919b1f461e6ea434fcbcc1d9d41996a900670857
John Dave Lagdameo <jdlagdameo@pldtglobal.com>
2021-02-05 08:21:31 +0800

    debug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,8 +12,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function register()
-    {
+    {   
         var_dump(\Request::secure());
+        var_dump( (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+        || $_SERVER['SERVER_PORT'] == 443);
         //
         if($this->app->environment('production')) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
